6e912e00b0428eccf550715a670170df0afa2a18
haddadrafik258 <haddadrafik258@gmail.com>
2022-04-26 15:27:26 +0100

    int value

--- server.js
+++ server.js
@@ -1,19 +1,59 @@
-var express = require('express');
-var cors = require('cors');
-var bodyParser = require("body-parser");
-var app = express();
-var port = process.env.PORT || 3000;
+const express = require("express");
+const cors = require("cors");
 
-app.use(bodyParser.json());
-app.use(cors());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+const app = express();
 
-var Users = require('./Routes/Users');
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
 
-app.use('/users',Users);
+app.use(cors(corsOptions));
 
-app.listen(port,function(){
-    console.log("Server is running on port: "+port);
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// database
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync();
+// force: true will drop the table if it already exists
+// db.sequelize.sync({force: true}).then(() => {
+//   console.log('Drop and Resync Database with { force: true }');
+//   initial();
+// });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
 });
+
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+ 
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
+ 
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
